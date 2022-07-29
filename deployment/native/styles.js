89c74537d1cba2745cd4d1978cc3a518a3005360
Sandra Hernandez <sandra@gurucx.com>
2022-07-29 17:13:07 -0500

    hola Sandra 2

--- deployment/native/styles.js
+++ deployment/native/styles.js
@@ -1,9 +1,9 @@
-import * as style0 from "C:/Users/decsi/OneDrive/Documentos/Mendix/EcommerceGurucx-main/themesource/administration/native/main";
-import * as style1 from "C:/Users/decsi/OneDrive/Documentos/Mendix/EcommerceGurucx-main/themesource/nanoflowcommons/native/main";
-import * as style2 from "C:/Users/decsi/OneDrive/Documentos/Mendix/EcommerceGurucx-main/themesource/webactions/native/main";
-import * as style3 from "C:/Users/decsi/OneDrive/Documentos/Mendix/EcommerceGurucx-main/themesource/atlas_core/native/main";
-import * as style4 from "C:/Users/decsi/OneDrive/Documentos/Mendix/EcommerceGurucx-main/themesource/myfirstmodule/native/main";
-import * as style5 from "C:/Users/decsi/OneDrive/Documentos/Mendix/EcommerceGurucx-main/theme/native/main";
+import * as style0 from "C:/Users/sandr/OneDrive/Documentos/Mendix/AppEcommerceGuruCX/EcommerceGurucx/themesource/administration/native/main";
+import * as style1 from "C:/Users/sandr/OneDrive/Documentos/Mendix/AppEcommerceGuruCX/EcommerceGurucx/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/sandr/OneDrive/Documentos/Mendix/AppEcommerceGuruCX/EcommerceGurucx/themesource/webactions/native/main";
+import * as style3 from "C:/Users/sandr/OneDrive/Documentos/Mendix/AppEcommerceGuruCX/EcommerceGurucx/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/sandr/OneDrive/Documentos/Mendix/AppEcommerceGuruCX/EcommerceGurucx/themesource/myfirstmodule/native/main";
+import * as style5 from "C:/Users/sandr/OneDrive/Documentos/Mendix/AppEcommerceGuruCX/EcommerceGurucx/theme/native/main";
 
 import { flatten } from "mendix/native";
 
